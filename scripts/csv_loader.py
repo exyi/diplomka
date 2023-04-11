aa51a48f8d1bb1bf21f618bc82bd135e9f59819f
Standa Lukeš <exyi@outlook.cz>
2023-04-11 11:50:52 +0200

    init scripts

---
+++ scripts/csv_loader.py
@@ -0,0 +1,224 @@
+from typing import Dict, Tuple
+import pandas as pd, numpy as np
+import sys
+import os
+import argparse
+import duckdb
+from collections import defaultdict
+
+
+parser = argparse.ArgumentParser(description='Parse CSVs')
+parser.add_argument('--path', type=str, help='directory containing CSVs to load', required=True)
+parser.add_argument('--out', type=str, help='output file', required=True)
+
+nucleotide_mapping = defaultdict(lambda: 'X', {
+    'A': 'A', 'C': 'C', 'G': 'G', 'T': 'T', 'U': 'U',
+    'DA': 'A', 'DC': 'C', 'DG': 'G', 'DT': 'T', 'DU': 'U',
+
+    '5MC': 'C', '2MG': 'G'
+})
+
+basic_nucleotides = [ "A", "T", "U", "G", "C", "X", " " ]
+ntcs = [
+    "NANT", # 2738272
+    "AA00", # 2880801
+    "AA08", # 1253456
+    "AA01", # 290652
+    "BB00", # 248604
+    "AA04", # 196233
+    "AB05", # 129296
+    "AA12", # 100281
+    "AA06", # 87112
+    "AA03", # 67976
+    "AA10", # 64719
+    "AA05", # 61008
+    "BB01", # 59435
+    "BB04", # 55360
+    "AA11", # 47708
+    "AA09", # 42467
+    "ZZ01", # 39024
+    "BB07", # 37621
+    "OP03", # 34898
+    "OP11", # 33172
+    "OP04", # 31279
+    "AA02", # 25176
+    "AB01", # 25119
+    "BA05", # 24962
+    "IC01", # 24854
+    "OP12", # 24755
+    "AA07", # 22712
+    "OP15", # 22564
+    "AB04", # 20022
+    "BB10", # 16235
+    "AA13", # 15698
+    "AAS1", # 15057
+    "OP08", # 13925
+    "BB02", # 13591
+    "OP24", # 13215
+    "BA01", # 12341
+    "BA08", # 11688
+    "IC02", # 9702
+    "BB16", # 9412
+    "OP20", # 9392
+    "OP10", # 9345
+    "OP07", # 8782
+    "OP09", # 8708
+    "OPS1", # 8596
+    "AB2S", # 8011
+    "BB15", # 7969
+    "OP13", # 7736
+    "OP01", # 7657
+    "OP21", # 7338
+    "AB03", # 7028
+    "OP23", # 6720
+    "OP26", # 6712
+    "OP18", # 6086
+    "OP06", # 6070
+    "OP29", # 6069
+    "OP14", # 5621
+    "BA13", # 5086
+    "BA16", # 5065
+    "OP02", # 4809
+    "OP22", # 4800
+    "OP31", # 4765
+    "OP05", # 4695
+    "BB03", # 4664
+    "IC03", # 4161
+    "IC04", # 3978
+    "OP28", # 3876
+    "BA10", # 3750
+    "BBS1", # 3479
+    "OP16", # 3256
+    "BB12", # 3239
+    "ZZ1S", # 2881
+    "BB13", # 2582
+    "OP25", # 2512
+    "BB08", # 2484
+    "OP17", # 2274
+    "OP30", # 2111
+    "BA17", # 2067
+    "BB11", # 1910
+    "BB17", # 1904
+    "AB02", # 1793
+    "BA09", # 1739
+    "OP27", # 1582
+    "ZZ2S", # 1453
+    "IC07", # 1444
+    "IC06", # 1325
+    "BB14", # 1305
+    "BB05", # 1105
+    "ZZ02", # 1024
+    "ZZS1", # 480
+    "IC05", # 326
+    "BB2S", # 326
+    "ZZS2", # 147
+    "OP19", # 108
+    "BB20", # 81
+    "OP1S", # 81
+    "BB1S", # 80
+    "AB1S", # 33
+]
+ntc_index = { word: index for index, word in enumerate(ntcs) }
+nucleotide_index = { nucleotide: index for index, nucleotide in enumerate(basic_nucleotides) }
+
+def encode_ntcs(ntcs: np.ndarray) -> np.ndarray:
+    return np.vectorize(lambda ntc: ntc_index[ntc])(ntcs)
+
+def encode_nucleotides(nucleotides: str) -> np.ndarray:
+    return np.array([ nucleotide_index[nucleotide] for nucleotide in nucleotides ])
+
+def map_nucleotide(nucleotide: str):
+    if '.' in nucleotide:
+        nucleotide = nucleotide.split('.')[0]
+    return nucleotide_mapping[nucleotide]
+
+def load_csv_file(file, name) -> Tuple[pd.DataFrame, Dict[Tuple[str, str], dict]]:
+    table = pd.read_csv(file, sep=',', header='infer')
+    stepID: pd.Series[str] = table['step_ID']
+    table['pdbid'] = [ s.split('_')[0] for s in stepID ]
+    table['chain'] = [ s.split('_')[1] for s in stepID ]
+    table['nt_1'] = [ s.split('_')[2] for s in stepID ]
+    table['nt_2'] = [ s.split('_')[4] for s in stepID ]
+    table['ntix_1'] = [ s.split('_')[3] for s in stepID ]
+    table['ntix_2'] = [ s.split('_')[5] for s in stepID ]
+    # chains = list(set(zip(table.pdbid, table.chain)))
+
+    # create array columns for each column in table, grouped by pdbid and chain
+    # identity = lambda x: x
+    groups = dict(list(table.groupby(['pdbid', 'chain'])))
+    def f(v):
+        sequence = np.array([ v['nt_1'].array[0] ] + list(v['nt_2']))
+        is_dna = np.array([ 'd' == s[0].lower() for s in sequence ], dtype=np.bool_)
+        return {
+            'steps': v,
+            'sequence': ''.join([ map_nucleotide(n) for n in sequence ]),
+            'sequence_full': sequence,
+            'is_dna': is_dna,
+        }
+
+    groups2 = { k: f(v) for k, v in groups.items() }
+    return table, groups2
+
+def get_joined_arrays(chains: Dict[Tuple[str, str], dict]):
+    def insert_spacers(arr, spacer):
+        result = []
+        for i in range(len(arr)):
+            result.append(arr[i])
+            if i < len(arr) - 1:
+                result.append(spacer)
+        return result
+
+    cs = list(chains.values())
+    sequence = " ".join([ c['sequence'] for c in cs ])
+    sequence_full = np.concatenate(insert_spacers([ c['sequence_full'] for c in cs ], [ ' ' ]))
+    is_dna = np.concatenate(insert_spacers([ c['is_dna'] for c in cs ], [ False ]))
+
+    def join_arrays(arrays, zero_element, dtype=None):
+        arrays = insert_spacers(arrays, np.array([ zero_element, zero_element ], dtype=dtype))
+        return np.concatenate(arrays, axis=0, dtype=dtype)
+
+    print(sequence)
+    result = {
+        "sequence": sequence,
+        "sequence_full": sequence_full,
+        "is_dna": is_dna,
+        "NtC": join_arrays([ c['steps']['NtC'].to_numpy(np.str_) for c in cs ], "NANT", dtype=np.str_),
+        "nearest_NtC": join_arrays([ c['steps']['nearest_NtC'].to_numpy(np.str_) for c in cs ], "NANT", dtype=np.str_),
+        "CANA": join_arrays([ c['steps']['CANA'].to_numpy(np.str_) for c in cs ], "NAN"),
+    }
+    for k in [ "d1", "e1", "z1", "a2", "b2", "g2", "d2", "ch1", "ch2", "NN", "CC", "mu", "rmsd", "confalA", "confalG", "confalH" ]:
+        result[k] = join_arrays([ c['steps'][k].to_numpy(np.float32) for c in cs ], 0.0, dtype=np.float32)
+    return result
+
+
+def save_parquet(structs, file):
+    frame = pd.DataFrame(pd.concat(structs).groupby("name").apply(lambda x: x.to_dict(orient='records')), columns=['chains'])
+    frame.to_parquet(file)
+
+def load_csvs(path, output_file):
+    structs = []
+    dirs = os.listdir(path)
+    tmp_counter = 0
+    for ix, file in enumerate(dirs):
+        if file.endswith('.csv'):
+            print(f'Loading {ix+1:<8}/{len(dirs)}: {file}                                 ', end='')
+            structs.append(load_csv_file(os.path.join(path, file), name = os.path.splitext(file)[0]))
+            print('\r', end='')
+
+        if len(structs) > 500:
+            save_parquet(structs, f'./tmp_{tmp_counter}.parquet')
+            tmp_counter += 1
+            structs.clear()
+
+    save_parquet(structs, f'./tmp_{tmp_counter}.parquet')
+
+    duckdb.query(f"""
+        COPY (SELECT * FROM './tmp_*.parquet') TO '{output_file}' (FORMAT 'parquet');
+    """)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    df = load_csvs(args.path, args.out)
+
+    print(f"Loaded {len(df)} structures.                             ")
---
+++ scripts/test-torch.py
@@ -0,0 +1,162 @@
+import math
+import time
+from typing import Dict, List, Optional, Tuple
+import torch, torch.nn as nn, torch.nn.functional as F, torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd, numpy as np, os, sys
+import scripts.csv_loader as csv_loader
+# import torchtext
+import random
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+MAX_LENGTH = 1024
+
+
+class StructuresDataset(Dataset):
+    def __init__(self, dir: str, files: Optional[List[str]]):
+        self.dir = dir
+        if files is None:
+            self.files = [ d for d in os.listdir(dir) if d.endswith('.csv') ]
+        else:
+            self.files = files
+
+    def __len__(self):
+        return len(self.files)
+
+    # def convert_target(self, table: pd.DataFrame, chains: Dict[Tuple[str, str], dict]):
+        
+
+    # def convert_input(self, table, chains: Dict[Tuple[str, str], dict]):
+    #     return ([ s['sequence'] for s in chains.values() ], [ s['is_dna'] for s in chains.values() ])
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.dir, self.files[idx])
+        table, chains = csv_loader.load_csv_file(img_path, os.path.splitext(self.files[idx])[0])
+        joined = csv_loader.get_joined_arrays(chains)
+        # N x 6
+        input = { "sequence": csv_loader.encode_nucleotides(joined['sequence']), "is_dna": joined['is_dna'] }
+        target = {
+            "NtC": csv_loader.encode_ntcs(joined['NtC']),
+            # "CANA": joined['CANA']
+        }
+        return input, target
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, embedding_size, hidden_size):
+        super(EncoderRNN, self).__init__()
+        self.embedding_size = embedding_size
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, embedding_size)
+        self.rnn = nn.LSTM(embedding_size, hidden_size)
+
+    def forward(self, input):
+        embedded = self.embedding(input)
+        output, _ = self.rnn(embedded)
+        return output
+
+class Decoder(nn.Module):
+    def __init__(self, hidden_size, output_size, max_length=MAX_LENGTH):
+        super(Decoder, self).__init__()
+        self.hidden_size = hidden_size
+        self.max_length = max_length
+        self.output_size = output_size
+
+        self.out = nn.Linear(hidden_size, output_size)
+
+        # self.attn = nn.Linear(self.hidden_size, self.max_length)
+        # self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
+
+    def forward(self, encoder_outputs):
+        output = F.relu(encoder_outputs)
+        output = self.out(output).softmax(dim=1)
+        return output
+
+teacher_forcing_ratio = 0.5
+
+def train(
+    input_data: Dict[str, torch.Tensor],
+    target_data: Dict[str, torch.Tensor],
+    encoder: EncoderRNN, decoder: Decoder,
+    encoder_optimizer, decoder_optimizer,
+    criterion, max_length=MAX_LENGTH):
+
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    input_sequence = input_data['sequence']
+    target_ntc_sequence = target_data['NtC']
+    # print(input_sequence.shape, target_ntc_sequence.shape)
+
+    encoder_output = encoder(input_sequence)
+
+    encoder_output = encoder_output[:, 1:, :] # there is one less nucleotide than nucleotide
+
+    decoder_output = decoder(encoder_output)
+
+    # print(f"{decoder_output.shape=} {target_ntc_sequence.shape=}")
+    loss = criterion(torch.swapaxes(decoder_output, -1, -2), target_ntc_sequence)
+    # loss = criterion(decoder_output, F.one_hot(target_ntc_sequence, num_classes=decoder.output_size))
+
+    loss.backward()
+
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return loss.item()
+
+def trainIters(encoder, decoder, n_iters, print_every=10, plot_every=100, learning_rate=0.01):
+    start = time.time()
+    plot_losses = []
+    print_loss_total = 0  # Reset every print_every
+    plot_loss_total = 0  # Reset every plot_every
+
+    encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate)
+    criterion = nn.CrossEntropyLoss()
+
+    dataset = StructuresDataset("../data/sample/", None) # ["2d47_v41C35A23.csv"]
+    datasetLoader = DataLoader(dataset, shuffle=True)
+
+    iter = 1
+    for epoch_i in range(2000):
+        for input_tensor, target_tensor in datasetLoader:
+            # print(input_tensor, target_tensor)
+            loss = train(input_tensor, target_tensor, encoder,
+                         decoder, encoder_optimizer, decoder_optimizer, criterion)
+            print_loss_total += loss
+            plot_loss_total += loss
+
+            if iter % print_every == 0:
+                print_loss_avg = print_loss_total / print_every
+                print_loss_total = 0
+                print('%s (%d %d%%) %.4f' % (time.time() - start,
+                                            iter, iter / n_iters * 100, print_loss_avg))
+
+            if iter % plot_every == 0:
+                plot_loss_avg = plot_loss_total / plot_every
+                plot_losses.append(plot_loss_avg)
+                plot_loss_total = 0
+
+            iter += 1
+
+    # showPlot(plot_losses)
+
+
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
+
+
+if __name__ == "__main__":
+    trainIters(EncoderRNN(input_size=len(csv_loader.basic_nucleotides), embedding_size=123, hidden_size=124), Decoder(124, len(csv_loader.ntcs)), 100000)
