bfbe793173bdc39edbc561cd622f8b7d2c503110
Standa Lukeš <exyi@outlook.cz>
2023-05-11 21:01:28 +0200

    use layers instead of modules

--- model/ntcnetwork_tf.py
+++ model/ntcnetwork_tf.py
@@ -12,7 +12,7 @@ from hparams import Hyperparams
 
 TensorDict = Dict[str, tf.Tensor]
 
-class ResnetBlock(tf.Module):
+class ResnetBlock(layers.Layer):
     def __init__(self, in_channels, out_channels = None, window_size = 3, stride=1, dilation = 1, name=None) -> None:
         super(ResnetBlock, self).__init__(name=name)
         if out_channels is None:
@@ -43,8 +43,8 @@ class ResnetBlock(tf.Module):
             self.conv_bypass = None
         # TODO: L2 regularization of conv layers
 
-    @tf.Module.with_name_scope
-    def __call__(self, input: tf.Tensor) -> tf.Tensor:
+    # @tf.Module.with_name_scope
+    def call(self, input: tf.Tensor) -> tf.Tensor:
         x: Any = input
         x = self.bn1(x)
         x = tf.nn.relu(x)
@@ -68,7 +68,8 @@ def swapaxes(tensor, dim1, dim2):
     transpose[dim1], transpose[dim2] = transpose[dim2], transpose[dim1]
     return tf.transpose(tensor, transpose)
 
-class ConvEncoder(tf.Module):
+
+class ConvEncoder(layers.Layer):
     def __init__(self, input_size, channels = [64, 64], window_size = 3, kind = "resnet", name=None) -> None:
         """
         set of convolutional layers
@@ -79,9 +80,12 @@ class ConvEncoder(tf.Module):
             ResnetBlock(1, in_size, out_size, window_size)
             for in_size, out_size in zip(input_sizes, channels)
         ])
+        # self.convolutions.build(input_shape=(None, None, input_size))
 
-    @tf.Module.with_name_scope
-    def __call__(self, input: Union[tf.Tensor, tf.RaggedTensor], lengths: Optional[tf.Tensor] = None):
+        self.layers = [ *self.convolutions.layers ]
+
+    # @tf.Module.with_name_scope
+    def call(self, input: Union[tf.Tensor, tf.RaggedTensor], lengths: Optional[tf.Tensor] = None):
         x: Any = input
         isragged = isinstance(x, tf.RaggedTensor)
         if isragged:
@@ -95,7 +99,7 @@ class ConvEncoder(tf.Module):
             x = tf.RaggedTensor.from_tensor(x, lengths=lengths)
         return x
 
-class EncoderRNN(tf.Module):
+class EncoderRNN(layers.Layer):
     def __init__(self,
             embedding: tf.Module,
             embedding_size,
@@ -124,11 +128,15 @@ class EncoderRNN(tf.Module):
         else:
             self.rnn = None
 
-    @tf.Module.with_name_scope
-    def __call__(self, input):
+        self.layers = [ self.embedding, self.rnn ]
+
+    # @tf.Module.with_name_scope
+    def call(self, input: tf.RaggedTensor):
         if self.input_dropout > 0:
             input = tf.nn.dropout(input, rate=self.input_dropout)
         embedded = self.embedding(input)
+        tf.assert_equal(input.values.shape[-2], embedded.values.shape[-2])
+
         embedded = tf.nn.dropout(embedded, rate=self.dropout)
 
         if self.rnn is None:
@@ -138,7 +146,7 @@ class EncoderRNN(tf.Module):
         output = tf.nn.dropout(output, rate=self.dropout)
         return output
 
-class Decoder(tf.Module):
+class Decoder(layers.Layer):
     def __init__(self, hidden_size, output_size, attention = 0, name=None):
         super(Decoder, self).__init__(name=name)
         self.hidden_size = hidden_size
@@ -157,8 +165,10 @@ class Decoder(tf.Module):
             # self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
             assert False
 
-    @tf.Module.with_name_scope
-    def __call__(self, encoder_outputs: tf.RaggedTensor) -> tf.RaggedTensor:
+        self.layers = [ self.out ]
+
+    # @tf.Module.with_name_scope
+    def call(self, encoder_outputs: tf.RaggedTensor) -> tf.RaggedTensor:
         output: Any = tf.nn.relu(encoder_outputs)
         output = self.out(output)
         output = tf.nn.softmax(output)
@@ -185,7 +195,7 @@ class Network(tf.keras.Model):
                 0.01 if k == "NANT" else
                 1
                 for k, v in csv_loader.ntc_frequencies.items()
-            ], dtype=tf.float32)
+            ], dtype=self.compute_dtype)
         # weight=torch.Tensor([
             #     0.01 if k == "NANT" else
             #     clamp(1 / (v / 20_000), 0.2, 1)
@@ -196,16 +206,21 @@ class Network(tf.keras.Model):
 
     def call(self, input: TensorDict, whatever =None):
         # print(input["sequence"].shape, input["is_dna"].shape)
-        one_hot_seq = tf.one_hot(input["sequence"], depth=len(csv_loader.basic_nucleotides), dtype=tf.float32)
+        one_hot_seq = tf.one_hot(input["sequence"], depth=len(csv_loader.basic_nucleotides), dtype=self.compute_dtype)
         # tf.print("Sequence: ", input["sequence"])
         in_tensor = tf.concat([
             one_hot_seq,
-            tf.cast(tf.expand_dims(input["is_dna"], -1), tf.float32)
+            tf.cast(tf.expand_dims(input["is_dna"], -1), self.compute_dtype)
         ], axis=-1)
         assert in_tensor.shape[-1] == Network.INPUT_SIZE
         assert in_tensor.shape[-2] == None
 
-        encoder_output = in_tensor
+        encoder_output: tf.RaggedTensor = self.encoder(in_tensor)
+        # tf.print("Input: ", in_tensor.shape, in_tensor.values.shape)
+        # tf.print("Encoder output: ", encoder_output.shape, encoder_output.values.shape)
+        tf.assert_equal(encoder_output.shape[-1], self.p.rnn_size)
+        tf.assert_equal(encoder_output.shape[-2], None)
+        tf.assert_equal(encoder_output.values.shape[-2], in_tensor.values.shape[-2])
         encoder_output = encoder_output[:, 1:, :] # there is one less NtC than nucleotides
         decoder_output = self.ntc_decoder(encoder_output)
         assert decoder_output.shape[-1] == len(csv_loader.ntcs)
@@ -218,7 +233,7 @@ class Network(tf.keras.Model):
         }
 
     def ntcloss(self, target: tf.RaggedTensor, output: tf.RaggedTensor):
-        target_onehot = tf.one_hot(target.values, depth=len(csv_loader.ntcs), dtype=tf.float32)
+        target_onehot = tf.one_hot(target.values, depth=len(csv_loader.ntcs), dtype=self.compute_dtype)
 
         loss = tf.losses.categorical_crossentropy(
             target_onehot,
