e2c36a8229ffb7cccdf766989570a2cf7afddda2
Standa Lukeš <exyi@outlook.cz>
2023-12-17 22:32:31 +0100

    Kostra diplomky

---
+++ diplomka/build.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+
+import subprocess, os, re, sys, json, dataclasses, argparse, shutil, typing as ty
+
+@dataclasses.dataclass
+class Options:
+    verbose: bool = False
+
+options = Options()
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+@dataclasses.dataclass
+class RunResult:
+    returncode: int
+    process: subprocess.Popen
+    stdout: ty.Optional[str]
+    stderr: ty.Optional[str]
+
+def format_cmd(cmd: ty.Sequence[str]):
+    return " ".join(arg if re.match(r'[-_/.a-z0-9]+', arg, re.IGNORECASE) else repr(arg) for arg in cmd)
+
+def run(name, *cmd, check=True, justexit=True, capture_output=False, allow_not_found=False):
+    if options.verbose:
+        eprint(f"{name}: {format_cmd(cmd)}")
+    try:
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE if capture_output else None, stderr=subprocess.PIPE if capture_output else None)
+    except FileNotFoundError as e:
+        if allow_not_found:
+            return RunResult(-1, False, None, None, None) #type: ignore
+        else:
+            raise e
+    out, err = None, None
+    if capture_output:
+        out, err = p.communicate()
+        out = out.decode("utf-8")
+        err = err.decode("utf-8")
+        if options.verbose:
+            eprint(f"{name} stdout:")
+            eprint(out)
+            eprint(f"{name} stderr:")
+            eprint(err)
+
+    p.wait()
+    if check and p.returncode != 0:
+        eprint(f"{name} failed [{cmd[0]}: {p.returncode}]")
+        eprint(f"cmd: {format_cmd(cmd)}")
+        if capture_output:
+            eprint("stdout:")
+            eprint(out)
+            eprint("stderr:")
+            eprint(err)
+        if justexit:
+            exit(13)
+        else:
+            raise Exception(f"{name} failed [{p.returncode}]")
+    return RunResult(p.returncode, p, out, err)
+
+def convert_pdfa(infile, outfile):
+    run("Convert to PDF/A with GhostScript", "gs", "-dPDFA", "-dBATCH", "-dNOPAUSE", "-sColorConversionStrategy=UseDeviceIndependentColor", "-sDEVICE=pdfwrite", "-dPDFACompatibilityPolicy=2", f"-sOutputFile={outfile}", infile, capture_output=True)
+    validate_pdfa(outfile)
+
+def validate_pdfa(file):
+    try:
+        r = run("PDF/A Verification", "verapdf", file, capture_output=True, check=False)
+    except FileNotFoundError:
+        eprint("verapdf not found, skipping PDF/A validation")
+        return False
+
+    if r.returncode != 0:
+        eprint("PDF/A validation failed")
+        eprint(r.stdout)
+        exit(1)
+    assert r.stdout is not None
+    if 'nonCompliant="0"' in r.stdout and 'compliant="1"' in r.stdout:
+        print("PDF/A validation passed")
+    else:
+        eprint("PDF/A validation is sus")
+
+def build_pdf(infile, outfile="out/thesis-{}.pdf"):
+    os.makedirs("out", exist_ok=True)
+    run("Typeset PDF with Chromium", "chromium", "--headless", "--disable-gpu", "--no-pdf-header-footer", "--virtual-time-budget=10000", "--print-to-pdf=" + outfile.format('chr'), infile)
+    try:
+        pdfa = convert_pdfa(outfile.format('chr'), outfile.format('pdfa'))
+    except Exception as e:
+        eprint("PDF/A conversion failed")
+        eprint(e)
+        pdfa = False
+    if pdfa:
+        return outfile.format('pdfa')
+    else:
+        return outfile.format('chr')
+    
+
+def human_sort_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    def atoi(text):
+        return int(text) if text.isdigit() else text
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+
+def pandoc_parse(input_dir, output_dir):
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    os.makedirs(output_dir)
+
+    out_files = []
+    for file in sorted(os.listdir(input_dir), key=human_sort_keys):
+        cmd = ["pandoc", "--to=json", "--output=" + os.path.join(output_dir, file + ".json")]
+        format = None
+
+        if file in ['metadata.yaml']:
+            continue
+        elif file.endswith(".md"):
+            format = "markdown+smart+gfm_auto_identifiers+superscript+subscript+inline_code_attributes+link_attributes+emoji"
+        elif file.endswith(".html"):
+            format = "html"
+        else:
+            raise Exception(f"Unknown file extension: {file}")
+        
+        cmd.append("--from=" + format)
+        cmd.append(os.path.join(input_dir, file))
+        run(f"Parse {file} with Pandoc", *cmd)
+        out_files.append(os.path.join(output_dir, file + ".json"))
+    return out_files
+
+def pandoc_render(files, output_file):
+    cmd = ["pandoc",
+           "--from=json", "--to=html",
+        #    "--embed-resources",
+           "--resource-path=text:html:images",
+           "--output=" + output_file,
+           "--metadata-file=text/metadata.yaml",
+           "--template=html/template.html",
+           "--standalone",
+        #    "--shift-heading-level-by=1",
+            "--lua-filter=html/shift-headings.lua",
+           "--toc", "--toc-depth=3",
+           "--number-sections", "--section-divs", "--katex"]
+    for file in files:
+        cmd.append(file)
+    run(f"Render {output_file} with Pandoc", *cmd)
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='Build the thesis')
+    parser.add_argument('--verbose', action='store_true', help='Verbose output')
+    args = parser.parse_args(argv)
+    options.verbose = args.verbose
+
+    files = pandoc_parse("text", "out/parsed")
+    pandoc_render(files, "out/thesis.html")
+
+    build_pdf("./html/thesis.html")
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ diplomka/text/2-oview-3-terminology.md
@@ -0,0 +1,55 @@
+## Base pair type terminology
+
+### Saenger system {.unnumbered}
+
+Since a large number of different non-canonical base pairs exists, a systematic way naming scheme is needed.
+The first system was described by W. Saenger in the Principles of Nucleic Acid Structure book published in 1984 [TODO cite].
+Seanger identified all possible base pairs with at least 2 hydrogen bonds and sorts them into 28 distinct classes.
+The classes are identified only be an ordinal number, but ordered the classes 
+
+TODO img https://www.nakb.org/basics/basepairs.html#Saenger/
+
+![Seanger's ](https://www.nakb.org/basics/BPImages/saenger.gif)
+
+### Leontis-Westhof system {.unnumbered}
+
+A more systematic approach was proposed (by N. B. Leontis and E. Westhof in a paper published in 2001)[https://pubmed.ncbi.nlm.nih.gov/11345429].
+They identified that each of the two nucleotides interacts with one of its three edges.
+The "front edge" is named Watson-Crick, since it is the only one binding in the canonical base pairing.
+One of the side edges is found binding in the Hoogsteen base pairing, so it is named Hoogsteen and shortened as H.
+Note that the Hoogsteen base pairing is actually binding of a Hoogsteen edge onto a Watson-Crick edge.
+The remaining edge is named Sugar, since this edge includes the covalent bond to ribose (N9-C1' or N1-C1').
+
+![Nucleotide edges](https://www.nakb.org/basics/BPImages/base-edges.jpg)
+
+A frequent misconception is that "Sugar" means the base binds to the ribose oxygen.
+Although this is frequently the case that the base pair **includes** a hydrogen bond to the O2' atom, it is definitely not necessary.
+The Sugar edge is primarily meant as one of purine/pyrimidine faces and most of the defined base pairs including the Sugar edge bind to an atom on the base, often the N3 purine atom.
+<!-- The corner atoms are included in the definition of both edges -- for example, the N2 guanine atom is shared between the Sugar and Watson-Crick edges. ??? -->
+
+TODO tSS GG
+
+Some base pairs defined by Westhof and Leontis do bind exclusively to the O2' ribose atom.
+However, this is the exception to the rule, and it makes us ask whether we shouldn't call these pairs "nucleotide pairs" instead of "base pairs".
+
+A pair of edges still leaves two possible conformations as one of the nucleotides can be rotated by 180°.
+Usually, this results in a different set of hydrogen bonds, so it is necessary to disambiguate the classes.
+The Leontis-Westhof naming system calls one conformation **cis** and the other **trans**.
+Cis essentially means that the N-C1' bonds point in the same direction (approximately, ± 180°).
+Trans is the other option, the N-C1' bonds point in opposite directions.
+
+Example: cWW GC, tWW GC
+
+The named are commonly shortened to 3 letters - `c` or `t` for cis or trans, and `W`, `H` or `S` for each of the 2 edges. `W` is Watson-Crick, `H` Hoogsten and `S` is the Sugar edge.
+Since the pairing conformation is different for each pair of base, we also include two `A`/`T`/`U`/`G`/`C` letters to identify them.
+For example, we can say that the image above shows the `cWW GC` and `tWW GC` pairs.
+
+### ?? {.unnumbered}
+
+As far as we are away, all publications today use the Leontis-Westhof system.
+In our opinion, the main disadvantage of the Saenger system is the need to remember the 28 classes.
+The Leontis-Westhof system is also more general, of the 156 (??) possible classes we can observe 122 in high quality X-ray structures deposited in PDB. 
+
+Being more general mainly means including pairs which some might not want to call "base pairs".
+A number of described base pairs only bind with a single hydrogen bonds or requires binding to ribose O2', restricting the class to RNA.
+However, a 2-bonded legit base pairs are missing in the Saenger system, for example the cWw GG.
